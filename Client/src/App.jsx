8c7feddffb73b7b879e8b28ed6ef1dda4977c33b
ivaniraldi <iraldiban@gmail.com>
2025-02-14 01:25:29 -0300

    Cambio del logo, actualizacion con TailwindCSS Y material-tailwind

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      
+      <h1 className='font-coderush text-5xl text-center text-violeta'>CodeRush - Develop in Progress</h1>
     </>
   )
 }
